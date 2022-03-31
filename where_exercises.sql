c42aa1a59c93c647e48d1fd01bb846bcd396dff8
Ray Cerna <ray.cerna01@gmail.com>
2022-03-31 13:41:13 -0500

    updating employee answers

---
+++ where_exercises.sql
@@ -0,0 +1,52 @@
+#1
+USE employees;
+#2. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. 
+#Enter a comment with the number of records returned. 477
+SELECT COUNT(DISTINCT first_name, last_name, birth_date)
+FROM employees
+where first_name in ('Irena','Vidaya','Maya');
+
+#3. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', 
+#as in Q2, but use OR instead of IN. 
+#Enter a comment with the number of records returned. 
+#Does it match number of rows from Q2?
+SELECT COUNT(DISTINCT first_name, last_name, birth_date)
+FROM employees
+where first_name = ('Irena')
+OR first_name = ('Vidya')
+OR first_name = ('Maya');
+#709, not it does not match
+
+#4. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya',
+#using OR, and who is male. Enter a comment with the number of records returned.
+SELECT DISTINCT first_name, gender
+FROM employees
+WHERE gender = 'M'
+AND first_name = ('Irena') 
+OR first_name = ('Vidya')
+AND gender = 'M'
+OR first_name = ('Maya')
+AND gender = 'M';
+SELECT COUNT(DISTINCT first_name, gender)
+FROM employees
+WHERE first_name = ('Irena') 
+AND gender = 'm'
+OR first_name = ('Vidya')
+AND gender = 'm'
+OR first_name = ('Maya')
+AND gender = ('M');
+#3
+
+#5. Find all current or previous employees whose last name starts with 'E'. 
+#Enter a comment with the number of employees whose last name starts with E.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name
+LIKE 'E%';
+
+SELECT COUNT(DISTINCT last_name)
+FROM employees
+WHERE last_name
+LIKE 'E%';
+#40
+
