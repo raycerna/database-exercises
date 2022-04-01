c6051b13360e4efc3f36b677cb946e59eda95d87
Ray Cerna <ray.cerna01@gmail.com>
2022-04-01 11:12:01 -0500

    completed functions_exercises

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -93,6 +93,49 @@ AND birth_date LIKE '%-12-25'
 ORDER by hire_date ASC, birth_date DESC;
 #Alselm Cappello
 
-test
+
+#1 Copy the order by exercise and save it as functions_exercises.sql.
+
+#2 Write a query to to find all employees whose last name starts and ends with 'E'. 
+#Use concat() to combine their first and last name together as a single column named full_name.
+
+
+SELECT CONCAT(first_name,last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+#3 Convert the names produced in your last query to all uppercase.
+
+SELECT UPPER(CONCAT(first_name,last_name)) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+#4 Find all employees hired in the 90s and born on Christmas. 
+#Use datediff() function to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE()),
+
+SELECT DATEDIFF(current_date(),hire_date) AS datediff, first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25';
+
+#5 Find the smallest and largest current salary from the salaries table.
+
+SELECT min(salary) AS smallest, max(salary) AS largest
+FROM salaries;
+
+#6 Use your knowledge of built in SQL functions to generate a username for all of the employees. 
+#A username should be all lowercase, and consist of the first character of the employees first name, 
+#the first 4 characters of the employees last name, an underscore, the month the employee was born, 
+#and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
+#username(f_initial,l_firstfour,mm,last2year),first_name,last_name,birth_date
+
+SELECT concat(substr(first_name,1,1),substr(last_name,1,4),'_',substr(birth_date,6,2),substr(birth_date,3,2)) AS user_name,
+first_name,last_name, birth_date
+FROM employees;
+
+
+
+
+
 
 
