610d6f745363e2a91ada26e39505ee8505747397
Ray Cerna <ray.cerna01@gmail.com>
2022-04-06 14:37:20 -0500

    adding case_exercises

---
+++ case_exercises.sql
@@ -0,0 +1,23 @@
+-- Create a file named case_exercises.sql and craft queries to return 
+-- the results for the following criteria:
+
+# 1. Write a query that returns all employees, their department number, 
+## their start date, their end date, and a new column 'is_current_employee' 
+### that is a 1 if the employee is still with the company and 0 if not.
+
+
+
+#2. Write a query that returns all employee names (previous and current), 
+## and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z'
+### depending on the first letter of their last name.
+
+
+
+
+#3. How many employees (current or previous) were born in each decade?
+
+
+
+#4. What is the current average salary for each of the following 
+## department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, 
+### Customer Service?
